2854c694f2a1e58b0653f307f7d7537ec7435ef4
Hussein Shoqanebi <97353378+theshoqanebi@users.noreply.github.com>
2022-09-01 19:38:53 +0300

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# cxxdroid-libraries-free
+Guide to get paid `Cxxdroid` (Android Application) libraries for free without modifying or installing mod apk
+
+This guide works for poth `arm` and `arm64` architectures, Tested on version `5.0`.
+
+Allegro version `5.2.7`
+
+SFML version `2.5.1`
+## Instructions:
+### 1. Install `Cxxdroid` from [`PlayeStore`](https://play.google.com/store/apps/details?id=ru.iiec.cxxdroid)
+### 2. To install the libraries:-
+a. For `Allegro` run this single command in `Cxxdroid` terminal
+```
+wget https://raw.githubusercontent.com/theshoqanebi/cxxdroid-libraries-free/main/allegro.sh -O - | sh
+```
+b. For `SFML` run this single command in `Cxxdroid` terminal
+```
+wget https://raw.githubusercontent.com/theshoqanebi/cxxdroid-libraries-free/main/sfml.sh -O - | sh
+```
---
+++ sfml.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/sh
+printf "\e[36m%s\e[0m\n" "Cxxdroid SFML Library Installation:-"
+printf "\e[35m%s" "* Checking support: "
+case `arch` in
+	aarch64)
+		printf "\e[32m%s\e[0m\n" "Supported";
+		archurl="arm64" ;;
+	arm)
+		archurl="arm" ;;
+	*)
+		printf "\e[30m%s\e[0m\n" "Unsuppored";
+		printf "\e[30m%s\e[0m\n" "* sfml library installation faild )-:"
+		exit 1 ;;
+	esac
+printf "\e[35m%s" "* Downloading Files: "
+wget "https://cxxdroid-libraries-free.herokuapp.com/?lib=sfml&arch=${archurl}" -O "/data/user/0/ru.iiec.cxxdroid/app_HOME/sfml-${archurl}.tar.xz" >/dev/null 2>&1
+printf "\e[32m%s\n" "Done"
+
+printf "\e[35m%s" "* Installing Library: "
+tar -xf "/data/user/0/ru.iiec.cxxdroid/app_HOME/sfml-${archurl}.tar.xz" -C /data/user/0/ru.iiec.cxxdroid/files/sysroot >/dev/null 2>&1
+printf "\e[32m%s\n" "Done"
+
+printf "\e[35m%s" "* Removing Temp Files: "
+rm "/data/user/0/ru.iiec.cxxdroid/app_HOME/sfml-${archurl}.tar.xz"
+printf "\e[32m%s\n" "Done"
+
+printf "\e[32m%s\e[0m\n" "* SFML library installed successfully (-:"
